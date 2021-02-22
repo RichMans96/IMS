cd806197eb47bbf1fa3162afdf7063077bd26077
RichMans96 <richard.mansworth@hotmail.co.uk>
2021-02-22 13:45:08 +0000

    CODE(IP-4) #comment added database #done(inprogress-to-done)

--- src/main/resources/sql-schema.sql
+++ src/main/resources/sql-schema.sql
@@ -10,3 +10,21 @@ CREATE TABLE IF NOT EXISTS `ims`.`customers` (
     `surname` VARCHAR(40) DEFAULT NULL,
     PRIMARY KEY (`id`)
 );
+
+CREATE TABLE IF NOT EXISTS `ims`.`items` (
+    `item_id` INT(11) NOT NULL AUTO_INCREMENT,
+    `item_name` VARCHAR(40) DEFAULT NULL,
+    `item_price` DOUBLE(8, 2) DEFAULT NULL,
+    PRIMARY KEY (`item_id`) 
+);
+
+CREATE TABLE IF NOT EXISTS `ims`.`orders` (
+   `order_id` INT(11) NOT NULL AUTO_INCREMENT,
+   `id` INT(11),
+   `item_id` INT(11),
+   `qty` INT (10),
+   `total_cost` DOUBLE(12, 2),
+   PRIMARY KEY (`order_id`),
+   FOREIGN KEY (`id`) REFERENCES customers(`id`),
+   FOREIGN KEY (`item_id`) REFERENCES items(`item_id`)
+);
