8fa38e4ec3906b331beefa22c9fbdd2a84761c4d
RichMans96 <richard.mansworth@hotmail.co.uk>
2021-02-23 17:13:05 +0000

    IP-10 #comment have added delete functionality, move to testing

--- src/main/java/com/qa/ims/persistence/dao/OrderDAO.java
+++ src/main/java/com/qa/ims/persistence/dao/OrderDAO.java
@@ -112,8 +112,15 @@ public class OrderDAO implements Dao<Order> {
 	}
 
 	@Override
-	public int delete(long id) {
-		// TODO Auto-generated method stub
+	public int delete(long orderId) {
+		try (Connection connection = DBUtils.getInstance().getConnection();
+				PreparedStatement statement = connection.prepareStatement("DELETE FROM orders WHERE order_id = ?");) {
+			statement.setLong(1, orderId);
+			return statement.executeUpdate();
+		} catch (Exception e) {
+			LOGGER.debug(e);
+			LOGGER.error(e.getMessage());
+		}
 		return 0;
 	}
 
